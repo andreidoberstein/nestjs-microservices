8dbadaa9da11240f08375badd626fe39b2039130
Andrei <andrei.vupt@gmail.com>
2025-10-05 16:56:59 -0300

    feat: common library | database & config module

---
+++ libs/common/src/database/database.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
+import { ConfigModule } from '../config/config.module'
+
+@Module({
+  imports: [MongooseModule],
+  // imports: [MongooseModule.forRootAsync({
+  //   imports: [ConfigModule],
+  //   useFactory: (configService: ConfigService) => ({
+  //     uri: configService.get('MONGO_URI'),
+  //   }),
+  //   inject: [ConfigService],
+  // }),
+  // ],
+})
+export class DatabaseModule {}
