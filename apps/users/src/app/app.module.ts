bd0d90e62ffe27237fa0615affa557e20a61397e
Andrei <andrei.vupt@gmail.com>
2025-10-30 15:52:25 -0300

    feat: serve users

--- apps/users/src/app/app.module.ts
+++ apps/users/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {ConfigModule, LoggerModule} from "@app/common";
 import {MongooseModule} from "@nestjs/mongoose";
+import {UsersController} from "./users/users.controller";
+import {UsersModule} from "./users/users.module";
 
 @Module({
   imports: [
     ConfigModule,
     LoggerModule,
     MongooseModule.forRoot(process.env.MONGODB_URI as string),
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -1,21 +1,21 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
-}
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const port = process.env.PORT_USERS || 3001;
+  await app.listen(port);
+  console.log(`[users] running on http://localhost:${port}`);
+}
 bootstrap();
